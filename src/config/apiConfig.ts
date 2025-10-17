0733d34ef0845cd5d69ba0295eaaf3b0644d3cc5
QwertyMD <aaryandhakal2020@gmail.com>
2025-10-17 10:33:07 +0545

    feat: implement event section with dummy data - Added event section with list of events and their details using dummy data

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -2,7 +2,7 @@ export const API_BASE_URL = 'http://localhost:3000/api';
 
 export const API_ENDPOINTS = {
   members: `${API_BASE_URL}/members`,
-
   projects: `${API_BASE_URL}/projects`,
   tags: `${API_BASE_URL}/tags`,
+  events: `${API_BASE_URL}/event`,
 };
--- src/pages/admin/AdminEvents.tsx
+++ src/pages/admin/AdminEvents.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/services/admin/eventServices";
+import EventCard from "@/components/EventCard";
 
 const AdminEvents = () => {
-  return (
-    <div>AdminEvents</div>
-  )
-}
+  const [events, setEvents] = useState([]);
 
-export default AdminEvents
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const data = await getAllEvents();
+        setEvents(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchEvents();
+  }, []);
+
+  // if (!events || events.length === 0) {
+  //   return <div className="p-8">Loading events...</div>;
+  // }
+
+  return (
+    <div className="grid py-12 px-8 gap-4">
+      <p className="text-4xl font-semibold">Events</p>
+
+      <Button className="justify-self-end">Add New Event</Button>
+
+      <div className="">Search and Filter</div>
+
+      <div className="flex flex-wrap gap-x-4 gap-y-12">
+        <EventCard 
+        // events={events} 
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AdminEvents;
